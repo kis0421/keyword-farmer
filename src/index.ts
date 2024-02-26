3b5037770fe86b21af7b5398bbef76e151d6fb73
kis0421 <kig0421@gmail.com>
2024-02-26 22:29:03 +0900

    feat keywordLength option range

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import { keywords } from './assets/ko/keywords'
 
 interface Option {
   loop?: boolean,
-  keywordLength?: number,
+  keywordLength?: number | { min?: number, max?: number },
   excludeSpaces?: boolean,
 }
 const useKeywordFarm = (option?: Option) => {
   const currentKeyword = option ? keywords.filter((keyword) => {
-    return option.keywordLength ? option.keywordLength === keyword.length : true
+    return option.keywordLength !== undefined ?
+      typeof option.keywordLength === 'object' ?
+        keyword.length >= (option.keywordLength?.min || 1) && keyword.length <= (option.keywordLength.max || Infinity)
+        : option.keywordLength === keyword.length
+      : true
   }) : keywords
   const create = () => {
     return currentKeyword[Math.floor(Math.random() * currentKeyword.length)]
@@ -15,6 +19,5 @@ const useKeywordFarm = (option?: Option) => {
   return { create }
 }
 
-const { create } = useKeywordFarm();
 
 export { useKeywordFarm }
