c9b0528c9c4abad0dbf8b14aefbfc58214a0a3cd
Minkyu Kim <kig0421@gmail.com>
2024-02-27 18:54:00 +0900

    style lint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { keywords } from './assets/ko/keywords'
+import { keywords } from './assets/ko/keywords';
 
 interface Option {
   loop?: boolean
@@ -9,38 +9,38 @@ const useKeywordFarm = (option?: Option) => {
   const currentKeyword = option === undefined
     ? keywords
     : keywords.reduce<string[]>((previous, current) => {
-      let targetKeyword = ''
+      let targetKeyword = '';
 
       // option.keywordLength case
       if (option.keywordLength !== undefined) {
         if (typeof option.keywordLength === 'object') {
           if ((current.length >= (option.keywordLength?.min ?? 1)) && (current.length <= (option.keywordLength.max ?? Infinity))) {
-            targetKeyword = current
+            targetKeyword = current;
           }
         } else if (typeof option.keywordLength === 'number') {
           if (current.length === option.keywordLength) {
-            targetKeyword = current
+            targetKeyword = current;
           }
         } else {
           // TODO: error
         }
       } else {
-        targetKeyword = current
+        targetKeyword = current;
       }
 
       // option.excludeSpaces case
       if (option.excludeSpaces ?? false) {
-        current = current.replaceAll(' ', '')
+        current = current.replaceAll(' ', '');
       }
 
-      previous.push(targetKeyword)
-      return previous
-    }, [])
+      previous.push(targetKeyword);
+      return previous;
+    }, []);
 
   const create = () => {
-    return currentKeyword[Math.floor(Math.random() * currentKeyword.length)]
-  }
-  return { create }
-}
+    return currentKeyword[Math.floor(Math.random() * currentKeyword.length)];
+  };
+  return { create };
+};
 
-export { useKeywordFarm }
+export { useKeywordFarm };
