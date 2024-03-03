f82aba6791da89a0e0def724a472dfaaa6f63552
kis0421 <kig0421@gmail.com>
2024-03-03 23:04:14 +0900

    fix specialCrops option

--- src/index.ts
+++ src/index.ts
@@ -8,39 +8,38 @@ interface Option {
 }
 
 const useKeywordFarm = (option?: Option) => {
-  if (option?.specialCrops === 'only') {
-    return specialKeywords;
-  }
   const currentKeyword = option === undefined
     ? keywords
-    : keywords.reduce<string[]>((previous, current) => {
-      let targetKeyword = '';
+    : option?.specialCrops === 'only'
+      ? specialKeywords
+      : keywords.reduce<string[]>((previous, current) => {
+        let targetKeyword = '';
 
-      // option.keywordLength case
-      if (option.length !== undefined) {
-        if (typeof option.length === 'object') {
-          if ((current.length >= (option.length?.min ?? 1)) && (current.length <= (option.length.max ?? Infinity))) {
-            targetKeyword = current;
-          }
-        } else if (typeof option.length === 'number') {
-          if (current.length === option.length) {
-            targetKeyword = current;
+        // option.keywordLength case
+        if (option.length !== undefined) {
+          if (typeof option.length === 'object') {
+            if ((current.length >= (option.length?.min ?? 1)) && (current.length <= (option.length.max ?? Infinity))) {
+              targetKeyword = current;
+            }
+          } else if (typeof option.length === 'number') {
+            if (current.length === option.length) {
+              targetKeyword = current;
+            }
+          } else {
+            // TODO: error
           }
         } else {
-          // TODO: error
+          targetKeyword = current;
         }
-      } else {
-        targetKeyword = current;
-      }
 
-      // option.excludeSpaces case
-      if (option.excludeSpaces ?? false) {
-        current = current.replaceAll(' ', '');
-      }
+        // option.excludeSpaces case
+        if (option.excludeSpaces ?? false) {
+          current = current.replaceAll(' ', '');
+        }
 
-      previous.push(targetKeyword);
-      return previous;
-    }, []);
+        previous.push(targetKeyword);
+        return previous;
+      }, []);
 
   const create = (count?: number) => {
     return count !== undefined
